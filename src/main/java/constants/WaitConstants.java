977c86f5ca1d335350c338e32a55b046ad35a45c
ChimelaO <107826485+ChimelaO@users.noreply.github.com>
2024-10-17 18:27:36 -0500

    Update WaitConstants.java

--- src/main/java/constants/WaitConstants.java
+++ src/main/java/constants/WaitConstants.java
@@ -6,7 +6,7 @@ import java.time.Duration;
 
 public class WaitConstants {
 	public static final Duration WAIT_FOR_ELEMENT = Duration.ofSeconds(20);
-	public static final Duration WAIT_FOR_ELEMENT_TO_DISAPPEAR = Duration.ofSeconds(10);
+	public static final Duration WAIT_FOR_ELEMENT_TO_DISAPPEAR = Duration.ofSeconds(15);
 	public static final int WAIT_FOR_UPLOAD_TO_FINISH = 4000;
 	
 }
